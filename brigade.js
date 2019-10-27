5a0d09837df2e36e766a0467ff78c79258663c51
Jakob Ehn <jakob@ehn.nu>
2019-10-27 16:02:19 +0100

    add gitignore

--- brigade.js
+++ brigade.js
@@ -2,11 +2,16 @@ const { events, Job } = require("brigadier");
 
 events.on("push", async () => {
     var compileStep = new Job("compile", "mcr.microsoft.com/dotnet/core/sdk:3.0")
+    compileStep.tasks = [
+        "dotnet build",
+        "dotnet publish",
+      ];
     // var testStep = new Job("test", "example/tester:latest")
     // var tagStep = new Job("tag:, "example/releasetagger:latest")
     // We could continue on creating the remaining steps
   
     await compileStep.run()
+
     // await testStep.run()
     // await tagStep.run()
     // We could continue running the remaining steps
