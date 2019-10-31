a58b0deea1fa680d97f105a76a33d71de1969a34
Jakob Ehn <jakob@ehn.nu>
2019-10-31 23:08:52 +0100

    asd

--- brigade.js
+++ brigade.js
@@ -22,10 +22,31 @@ events.on("push", async () => {
     var hello = new Job("linux-job")
     hello.image = "alpine:3.4"
     hello.tasks = ["echo Hello Brigade from Azure"]
-
-
-    
     
     hello.run()    
+  });
+
+  events.on("simpleevent", (e, p) => {  // handler for a SimpleEvent
+    var echo = new Job("echosimpleevent", "alpine:3.8");
+    echo.tasks = [
+      "echo Project " + p.name,
+      "echo event type: $EVENT_TYPE"
+    ];
+    echo.env = {
+      "EVENT_TYPE": e.type
+    };
+    echo.run();
+  });
+  
+  events.on("cloudevent", (e, p) => { // handler for a CloudEvent
+    var echo = new Job("echocloudevent", "alpine:3.8");
+    echo.tasks = [
+      "echo Project " + p.name,
+      "echo event type: $EVENT_TYPE"
+    ];
+    echo.env = {
+      "EVENT_TYPE": e.type
+    };
+    echo.run();
   });
   
