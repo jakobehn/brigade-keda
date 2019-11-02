d7204649c07e7dee3b1439d72731fdacc89e17bc
Jakob Ehn <jakob@ehn.nu>
2019-11-02 15:23:35 +0100

    added helm

--- 3. CloudEvents/brigade.js
+++ 3. CloudEvents/brigade.js
@@ -1,4 +1,5 @@
 const { events } = require('brigadier')
+const { url } = require('url')
 
 events.on("Microsoft.Storage.BlobDeleted", (e, p) => {
   console.log("Received BlobDeleted event:")
@@ -9,9 +10,44 @@ events.on("Microsoft.Storage.BlobDeleted", (e, p) => {
 
 events.on("Microsoft.Storage.BlobCreated", (e, p) => {
   console.log("Received BlobCreated event:")
-  console.log(e.payload);
+
+  //console.log(e.payload);
   var pl = JSON.parse(e.payload);
-  console.log("parsed json");
-  console.log(pl);  
-  console.log(pl.eventType);
+  console.log(pl);
+  var accountInfo = parseAzureBlobUri(pl.data.url);
+  // console.log(accountInfo);
+
 })
+
+
+
+const parseAzureBlobUri = (blobUrl) => {
+    let uri = url.parse(blobUrl)
+
+    // Extract the storage account name
+    let storageAccountName = uri.hostname.split('.')[0]        
+
+    // Remove the 1st trailing slash then extract segments
+    let segments = uri.pathname.substring(1).split('/')
+
+    // If only one segment, this is the blob name
+    if(segments.length === 1){
+        return {
+            storageAccountName,
+            containerName: '$root',
+            blobName: segments[0]
+        }
+    }
+
+    // get the container name
+    let containerName = segments[0]
+
+    // Remove the containername from the segments
+    segments.shift()
+
+    return {
+        storageAccountName,
+        containerName,
+        blobName: segments.join('/')
+    }
+}
