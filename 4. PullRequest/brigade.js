35c7bc28b5326ab5d87b846fcd60b2f0265bd91a
Jakob Ehn <jakob@ehn.nu>
2019-11-03 11:19:11 +0100

    asd

---
+++ 4. PullRequest/brigade.js
@@ -0,0 +1,54 @@
+const { events, Job } = require("brigadier");
+const util = require('util')
+
+const HELM_VERSION = "v2.13.0"
+const HELM_CONTAINER = "lachlanevenson/k8s-helm:" + HELM_VERSION;
+
+const KUBECTL_VERSION = "v1.12.8";
+const KUBECTL_CONTAINER = "lachlanevenson/k8s-kubectl:" + KUBECTL_VERSION;
+
+events.on("simpleevent", (event, project) => {
+    const payload = JSON.parse(event.payload);
+    const prId = payload.resource.pullRequestId;
+
+    if (!payload.resource.sourceRefName.includes('/feature/') && !payload.resource.sourceRefName.includes('/bug/')) {
+        console.log(`The source branch ${payload.resource.sourceRefName} is not a /feature/ or /bug/ and is therefore skipped.`)
+        return;
+    }
+
+    if (payload.resource.status !== "completed" && payload.resource.status !== "abandoned") {
+        console.log(`PullRequest not complete or abandoned (current status: ${payload.resource.status}).`);
+        return;
+    }
+
+    var helm_job = new Job("helm-job", HELM_CONTAINER);
+    helm_job.env = {
+        'HELM_HOST': project.secrets.helmHost
+    };
+    helm_job.tasks = ["helm init --client-only", `helm delete --purge samplewebapp-${prId}`];
+
+    var kubectl_job = new Job("kubectl-job", KUBECTL_CONTAINER);
+    kubectl_job.tasks = [`kubectl delete namespace samplewebapp-${prId}`];
+
+    console.log("==> Running helm_job Job")
+    helm_job.run().then(helmResult => {
+        console.log("==> helm job results")
+        console.log(helmResult.toString())
+        console.log("==> helm job done")
+
+        kubectl_job.run().then(kubectlResult => {
+            console.log("==> kubectl job results");
+            console.log(kubectlResult.toString());
+            console.log("==> kubectl job done");
+        });
+    })
+});
+
+events.on("error", (e) => {
+    console.log("Error event " + util.inspect(e, false, null))
+    console.log("==> Event " + e.type + " caused by " + e.provider + " cause class" + e.cause + e.cause.reason)
+})
+
+events.on("after", (e) => {
+    console.log("After event fired " + util.inspect(e, false, null))
+});
