429e1ad445122f5e5d7f26bee2b4e55435b65b1d
Jakob Ehn <jakob@ehn.nu>
2019-11-04 22:33:06 +0100

    sdf

--- 2 GitHub push/brigade.js
+++ 2 GitHub push/brigade.js
@@ -1,12 +1,33 @@
-const { events, Job } = require("brigadier");
+const { events, Job, Group } = require("brigadier");
 
-events.on("push", async () => {
+events.on("push", async (e, p) => {
     var compileStep = new Job("compile", "mcr.microsoft.com/dotnet/core/sdk:3.0")
     compileStep.tasks = [
         "cd src",
         "dotnet build",
-        "dotnet publish",
       ];  
-  });
 
+      var testStep = new Job("test", "mcr.microsoft.com/dotnet/core/sdk:3.0")
+      testStep.tasks = [
+          "cd src",
+          "dotnet test",
+        ];  
+
+      var publishStep = new Job("publish", "mcr.microsoft.com/dotnet/core/sdk:3.0")
+      publishStep.tasks = [
+          "cd src",
+          "dotnet publish",
+        ];          
+
+      var slack = new Job("slack-notify", "technosophos/slack-notify:latest", ["/slack-notify"])
+      slack.env = {
+        SLACK_WEBHOOK: p.secrets.SLACK_WEBHOOK,
+        SLACK_USERNAME: "Brigade",
+        SLACK_TITLE: "sampleapp CI trigger",
+        SLACK_MESSAGE: "Sampleapp built and tested successfully!s"
+      }          
   
+    Group.runEach([compileStep,testStep,publishStep,slack])
+  
+    
+  });
