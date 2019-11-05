f0d0a8c264f7af4804aac5e37be3ca6cc9bab84d
Jakob Ehn <jakob@ehn.nu>
2019-11-05 13:02:13 +0100

    refactor

--- 3. CloudEvents/brigade.js
+++ 3. CloudEvents/brigade.js
@@ -1,14 +1,35 @@
-const { events } = require('brigadier')
-
-events.on("Microsoft.Storage.BlobDeleted", (e, p) => {
-  console.log("Received BlobDeleted event:")
-  var str = JSON.stringify(JSON.parse(e.payload),null,2);  
-  console.log(str);
-})
+const { events, Job, Group } = require('brigadier')
 
 events.on("Microsoft.Storage.BlobCreated", (e, p) => {
-  echo("Received BlobCreated event:")
-  var str = JSON.stringify(JSON.parse(e.payload),null,2);  
-  console.log(str);
+  //Log received event
+  console.log("\n\nReceived BlobCreated event:\n\n")
+
+  //Parse event
+  var payLoad = JSON.parse(e.payload);
+  console.log(JSON.stringify(payLoad,null,2));  
+  
+  //Run brigade job
+  processChangedFile(payLoad.data.url);
 })
+
+events.on("Microsoft.Storage.BlobDeleted", (e, p) => {
+  //Log received event  
+  console.log("\n\nReceived BlobDeleted event:\n\n")
+
+  //Parse event  
+  var payLoad = JSON.parse(e.payload);
+  console.log(JSON.stringify(payLoad,null,2));  
+  
+  //Run brigade job
+  processChangedFile(payLoad.data.url);
+})
+
+function processChangedFile(url) {
+  var job = new Job("process-storage-file", "microsoft/azure-cli");
+  job.env = { STORAGE_URL: url}
+  job.tasks = ["echo Processing created file: $STORAGE_URL"];
+  job.run();    
+}
+
+
 
