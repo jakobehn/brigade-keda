daf880c26ac11424204056e23810b3ee14c82425
Jakob Ehn <jakob@ehn.nu>
2019-11-06 09:52:07 +0100

    asd

--- cleanup.ps1
+++ cleanup.ps1
@@ -2,3 +2,6 @@ brig build delete-all jakob/pr
 brig build delete-all jakob/brigadedemo
 brig build delete-all jakob/cloudevents
 brig build delete-all jakobehn/brigadeapp
+
+k delete pod --field-selector=status.phase==Succeeded
+k delete pod --field-selector=status.phase==Failed
